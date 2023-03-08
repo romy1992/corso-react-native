e8111fc92d765c1dcb7686783d84344bcc0286a2
Trotta Rosario <appero.app@gmail.com>
2023-03-08 09:00:56 +0100

    first commint

--- corso/App.js
+++ corso/App.js
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [goalText, text] = useState('');
+
+  function inputChange(inputText) {
+    text(inputText);
+  }
+
+  function pressInput() {
+    if (goalText === 'rosario')
+      console.log(goalText);
+    else
+      console.log('noooo');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app</Text>
-      <StatusBar style="auto" />
+      <View style={styles.contentInput}>
+        <TextInput style={styles.textInput} placeholder="Inserisci qualcosa" onChangeText={inputChange} />
+        <Button title="Aggiungi" onPress={pressInput} />
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>Lista ....</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +32,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 16
   },
+  contentInput: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingBottom: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccccc'
+  },
+  textInput: {
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 10,
+    padding: 8
+  },
+  goalsContainer: {
+    marginTop: 20,
+    flex: 10,
+  }
 });
