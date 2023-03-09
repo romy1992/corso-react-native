502b7fff1b3820dba707d8f57635b18c51d6a726
Rosario Trotta <trottarosario@gmail.com>
2023-03-09 18:26:06 +0100

    aggiunta di componente

--- corso/App.js
+++ corso/App.js
@@ -1,20 +1,22 @@
 import { useState } from 'react';
-import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, FlatList, StyleSheet, TextInput, View } from 'react-native';
+import GoalItem from './componets/GoalItem';
+
 
 export default function App() {
 
   // Gli hook sono una nuova aggiunta in React 16.8. Ti consentono di utilizzare lo stato e altre funzionalità di React senza scrivere una classe.
   // https://reactjs.org/docs/hooks-state.html
-  const [goalText, text] = useState('');
+  const [goalText, textGoal] = useState('');
   // E' una variabile di stato composta da : 
   // 1 goalText =>Nome variabile
-  // 2 text => Valore Vatiabile
+  // 2 textGoal => Valore Vatiabile
   // 3 useState('CIAO') => Stato iniziale della variabile se si clicca "vuoto"..In questo caso stampa 'CIAO' se l'input è vuoto
 
   const [listGoals, setValueGoals] = useState([]);// Crea una nuova variabile di globale specificandone che è una lista "useState([])"
 
-  function inputChange(inputText) { // Una volta inserito il valore TextInput , onChangeText passerà il valore alla funzione iserita..In questo caso la variabile di stato viene settato nel text
-    text(inputText);
+  function inputChange(inputText) { // Una volta inserito il valore TextInput , onChangeText passerà il valore alla funzione iserita..In questo caso la variabile di stato viene settato nel textGoal
+    textGoal(inputText);
   }
 
   function pressInput() {
@@ -22,7 +24,12 @@ export default function App() {
     if (goalText === '')
       return alert("Non può può essere vuoto");
 
-    setValueGoals((valueGoal) => [...valueGoal, goalText]); // Setto in valore di quella lista "setValueGoals" concatendandone(...nomeValoreLambda) il valore dell'inputText("text" della prima variabile di stato con nome variabile "goalText")
+    // Setto in valore di quella lista "setValueGoals" concatendandone(...nomeValoreLambda) il valore dell'inputText("text" della prima variabile di stato con nome variabile "goalText")
+    setValueGoals((valueGoal) => [
+      ...valueGoal, { text: goalText, id: Math.random().toString() },//"text" e "key" sono valori necessari per le liste..
+      //"text" => un nome a caso per recuperare il valore
+      //"key"(o altro) => nome STANDARD..in caso di modifica con nome custome vedere nella FLATLIST la proprietà keyExtractor
+    ]);
 
   }
 
@@ -33,13 +40,25 @@ export default function App() {
         <Button title="Aggiungi" onPress={pressInput} />
       </View>
       <View style={styles.goalsContainer}>
-        <ScrollView>
-          {/* Per poter spampare la lista, bisogna usare il metodo "map" specificandone il nomeLambda dove come esecuzione ci saranno label <Text> con "key"( Obbligatoria e magari con un numero random ) e valore dichiarato */}
+        {/* <ScrollView>
+          Per poter stampare la lista, bisogna usare il metodo "map" specificandone il nomeLambda dove come esecuzione ci saranno label <Text> con "key"( Obbligatoria e magari con un numero random ) e valore dichiarato 
           {listGoals.map((goal) =>
             <View style={styles.singleGoalView} key={goal}>
               <Text style={styles.goalText}>{goal}</Text>
             </View>)}
-        </ScrollView>
+        </ScrollView>*/}
+        <FlatList /** Flat list lavora sulle liste in maniera "Pigra" cioè, carica gli elementi nel momento in cui si scrolla e non tutti insieme come  ScrollView */
+          data={listGoals}/**Gli elementi che accetta la flatList sono DATA che sarebbe la lista da inserire */
+          renderItem={(itemData) => { /** renderItem è la seconda proprietà obbligatoria da aggiungere composta da : elemento singolo (itemData)*/
+            /**e dal Return che non è altro che la parte "visiva" */
+            return <GoalItem text={itemData.item.text} />;/**E' in nuovo componente aggiunto ..come proprietà possiamo mettetre un nome a caso , in questo caso "text" che verrà richiamato nella props del componete */
+            /**Gli elementi si recuperano con : nome del singono elemento (itemData) .item (nome STRANDARD) .nome varibile di come è stato dichiaranto nella lista, in questo caso "text" */
+          }}
+          keyExtractor={(item, index) => {
+            return (item.id)
+          }}
+          alwaysBounceVertical={false}/**Non obbligatorio, serve per non far "rimbalzare" la lista */
+        />
       </View>
     </View>
   );
@@ -72,13 +91,5 @@ const styles = StyleSheet.create({
     marginTop: 20,
     flex: 10,
   },
-  singleGoalView: {
-    padding: 4,
-    borderRadius: 5,
-    backgroundColor: 'blue',
-    marginTop: 10
-  },
-  goalText: {
-    color: 'white'
-  }
+
 });
---
+++ corso/componets/GoalItem.js
@@ -0,0 +1,26 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+// creo un nuovo componente in questo modo
+function GoalItem(props) {//(props = accesso alle proprietà che mi vengono passate dall'altra parte)
+    return (//Il solito return
+        <View style={styles.singleGoalView}>
+            <Text style={styles.goalText}>{props.text}</Text>{/* "props" richiamo per accesso alle proprietà .... "text" nome con lui dell'altra parte è stato dichiarato il nome della proprietà */}
+        </View>
+    );
+}
+
+// lo esporto in questo modo
+export default GoalItem;
+
+
+const styles = StyleSheet.create({
+    singleGoalView: {
+        padding: 4,
+        borderRadius: 5,
+        backgroundColor: 'blue',
+        marginTop: 10
+    },
+    goalText: {
+        color: 'white'
+    }
+});
