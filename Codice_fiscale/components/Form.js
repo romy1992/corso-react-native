00c849b8521f6c785b607e041e01e15b8a838a8b
Rosario Trotta <trottarosario@gmail.com>
2023-05-02 16:28:01 +0200

    codice fuscale terminato

--- Codice_fiscale/components/Form.js
+++ Codice_fiscale/components/Form.js
@@ -17,14 +17,11 @@ function Form({ isScreen, code }) {
 
     // Funzioni di settaggio form
     function checkTextInputSurname(valueText) {
-        valueText = setValueText(valueText);
-        return setTextInputSurname(subStringSurname(valueText))
+        return setTextInputSurname(valueText);
     }
 
     function checkTextInputName(valueText) {
-        valueText = setValueText(valueText);
-        console.log(valueText)
-        // return setTextInputName(subStringName(valueText))
+        return setTextInputName(valueText);
     }
 
     function checkTextInputCity(valueText) {
@@ -47,9 +44,17 @@ function Form({ isScreen, code }) {
                 [{ text: 'Attenzione!', style: 'cancel' }])
         }
 
+
+        let surname = subStringSurname(setValueText(textInputSurname));
+        let name = subStringName(setValueText(textInputName));
+        let date = selectedDate.substring(2, 4);
+        let month = charMonth();
+        let day = numDay();
+        let codCity = getCodeCity();
+        let codSecurity = 'B'; //TODO
+
         // Setta il codice
-        code(textInputSurname + textInputName
-            + selectedDate.substring(2, 4).toUpperCase());
+        code(surname + name + date + month + day + codCity + codSecurity);
 
         // Cambia lo screen dopo i controlli e il risultato del codice fiscale
         isScreen(false);
@@ -58,26 +63,62 @@ function Form({ isScreen, code }) {
     // Set value
     function setValueText(valueText) {
         valueText = valueText.trim().toUpperCase()
-            .replace("A", "")
-            .replace("E", "")
-            .replace("I", "")
-            .replace("O", "")
-            .replace("U", "");
+            .replaceAll("A", "")
+            .replaceAll("E", "")
+            .replaceAll("I", "")
+            .replaceAll("O", "")
+            .replaceAll("U", "");
         return valueText;
     }
 
     // SubString Surname 1-2-3 
     function subStringSurname(valueText) {
-        valueText = valueText.substring(0, 3);
+        valueText = valueText.replaceAll(' ', '').substring(0, 3);
         return valueText;
     }
 
     // SubString Name 1-3-4
     function subStringName(valueText) {
-        let first = valueText.substring(0, 1);
-        let total = first+valueText.trim().substring(3, 4);
-        // console.log(total)
-        return total;
+        let repl = valueText.replaceAll(' ', '');
+        let first = repl.substring(0, 1);
+        let total = first + repl.substring(2, 4);
+        return total.trim();
+    }
+
+    // Carattere del mese
+    function charMonth() {
+        let num = selectedDate.substring(5, 7);
+        if (num.startsWith(0))
+            num = num.replace('0', '')
+        switch (num) {
+            case '1': return 'A'
+            case '2': return 'B'
+            case '3': return 'C'
+            case '4': return 'D'
+            case '5': return 'E'
+            case '6': return 'H'
+            case '7': return 'L'
+            case '8': return 'M'
+            case '9': return 'P'
+            case '10': return 'R'
+            case '11': return 'S'
+            case '12': return 'T'
+        }
+    }
+
+    // Giorno di nascita
+    function numDay() {
+        let day = selectedDate.substring(8, 10);
+        // Se il sesso è femminile si aggiunge 40 al giorno di nascita
+        if (selectedValue === 'femmina')
+            day = day + 40;
+        return day;
+    }
+
+    function getCodeCity() {
+        if (textInputCity.toLocaleLowerCase() === 'manfredonia')
+            return 'E885';
+        return '1234'
     }
 
     return (
